cb31ec20e28b5db21bee484ac10d03ea40385e72
jack <laplante.jack@gmail.com>
2019-07-13 21:58:47 -0400

    multiplayer baybeee

--- server/server.js
+++ server/server.js
@@ -1,24 +1,20 @@
+// inspector for debugging
+const util = require('util')
+
 // WebSocket server
 const WebSocket = require('ws');
 
 const wss = new WebSocket.Server({ port: 18181 });
 
-var players = []
 
-function addPlayer(client) {
-    players[players.length] = client;
-}
-
-wss.on('connection', function connection(ws) {
-    addPlayer(ws)
+wss.on('connection', function connection(ws, req) {
     ws.on('message', function incoming(message) {
-        console.log('received: %s', message);
         message = JSON.parse(message)
-        players.forEach(function each(client, playerIndex) {
+        wss.clients.forEach(function each(client) {
             if (client !== ws && client.readyState === WebSocket.OPEN) {
-                message.player = playerIndex
                 client.send(JSON.stringify(message));
             }
+            console.log('sending: %s', JSON.stringify(message));
           });
     });
     // ws.send('something');
@@ -28,8 +24,10 @@ wss.on('connection', function connection(ws) {
 // static client server
 var static = require('node-static');
 var file = new static.Server('../client');
+var port = 8080
 require('http').createServer(function (request, response) {
     request.addListener('end', function () {
         file.serve(request, response);
     }).resume();
-}).listen(8080);
+}).listen(port);
+console.log("serving index.html at: http://localhost:"+8080);
