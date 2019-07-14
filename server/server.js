103f7d819ae002bdfc26aa7bf030b9df9985fee6
unknown <laplante.jack@gmail.com>
2019-07-14 18:47:54 -0400

    server keeps better track of players. player disconnect kinda works

--- server/server.js
+++ server/server.js
@@ -1,24 +1,45 @@
 // inspector for debugging
 const util = require('util')
 
+var players = {}
+
 // WebSocket server
 const WebSocket = require('ws');
-
 const wss = new WebSocket.Server({ port: 18181 });
-
-
 wss.on('connection', function connection(ws, req) {
+    sendMessage(ws, {players: players})
     ws.on('message', function incoming(message) {
         message = JSON.parse(message)
+        if(message.player){
+            var player = message.player
+            if (!ws.player) {
+                ws.player = player
+            }
+            if (message.x && message.z) {
+                players[player] = {
+                    x: message.x,
+                    z: message.z
+                }
+            }
+        }
         wss.clients.forEach(function each(client) {
             if (client !== ws && client.readyState === WebSocket.OPEN) {
-                client.send(JSON.stringify(message));
+                sendMessage(client, message);
             }
             console.log('sending: %s', JSON.stringify(message));
           });
     });
+    ws.on('close', function onClose(code, req){
+        console.log("player "+this.player+" disconnected");
+        delete players[this.player];
+    })
     // ws.send('something');
 });
+
+function sendMessage(client, message) {
+    client.send(JSON.stringify(message));
+    console.log('sending: %s', JSON.stringify(message));
+}
 
 
 // static client server
