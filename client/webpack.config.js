00d00e384dfd75e22e75d7a07581bb633fcf95df
Jack LaPlante <laplantej@google.com>
2019-07-19 12:01:00 -0400

    added webpack, TODO fix vox and add hot reloading

---
+++ client/webpack.config.js
@@ -0,0 +1,19 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+      rules: [
+        {
+            test: /\.(png|svg|jpg|gif|glb)$/,
+            use: [
+                'file-loader'
+            ]
+        }
+      ]
+  }
+};
