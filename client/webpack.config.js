fec7b41dc510801f33bf909edf50549a89eb89bf
Jack LaPlante <laplantej@google.com>
2019-07-19 15:24:43 -0400

    added inline-source-map

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,19 +1,21 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist')
-  },
-  module: {
-      rules: [
-        {
-            test: /\.(png|svg|jpg|gif|glb)$/,
-            use: [
-                'file-loader'
-            ]
-        }
-      ]
+    mode: 'development',
+    entry: './src/index.js',
+    devtool: 'inline-source-map',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(png|svg|jpg|gif|glb)$/,
+                use: [
+                    'file-loader'
+                ]
+            }
+        ]
   }
 };
