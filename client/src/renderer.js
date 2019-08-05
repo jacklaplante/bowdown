35b6062bfc1a88b93cac699e56e7397d4ea42fdf
Jack LaPlante <laplante.jack@gmail.com>
2019-08-05 11:20:00 -0400

    fixed resolution issue

--- client/src/renderer.js
+++ client/src/renderer.js
@@ -2,6 +2,7 @@ import {WebGLRenderer} from 'three'
 
 var renderer = new WebGLRenderer();
 renderer.setClearColor("#e5e5e5");
+renderer.setPixelRatio( window.devicePixelRatio );
 renderer.setSize( window.innerWidth, window.innerHeight );
 
 export  {renderer }
