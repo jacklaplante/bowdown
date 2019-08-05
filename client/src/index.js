961ce69c231cb4d545906316dac22b1e49fcb002
Jack LaPlante <laplante.jack@gmail.com>
2019-08-05 13:31:42 -0400

    moved player1 animations to player1.js

--- client/src/index.js
+++ client/src/index.js
@@ -11,11 +11,13 @@ var clock = new Clock()
 document.body.appendChild( renderer.domElement )
 var theta = 0
 var phi = 0
-var forward = false
-var backward = false
-var left = false
-var right = false
-var space = false
+var input = {
+    forward: false,
+    backward: false,
+    left: false,
+    right: false,
+    space: false
+}
 
 document.addEventListener('mousemove', onMouseMove);
 document.addEventListener('keydown', onKeyDown);
@@ -29,69 +31,7 @@ function animate() {
     requestAnimationFrame( animate );
     var delta = clock.getDelta();
     if (player1 && mixer) {
-        var nextPos = new Vector3();
-        var rotation;
-        if (player1.falling()) {
-            mixer.clipAction( player1.animations[1] ).fadeOut(0.2);
-            player1.state = 'falling'
-            player1.velocity.y -= delta*10
-            nextPos = player1.scene.position.clone().add(player1.velocity.clone().multiplyScalar(delta))
-            movePlayer1(nextPos, rotation)
-        } else {
-            player1.velocity.set(0,0,0)
-            if (space) {
-                player1.velocity.y = 5
-            }
-            if (forward || backward || left || right) {
-                var movementSpeed = 0.12;
-                var direction = new Vector3();
-                camera.getWorldDirection(direction)
-                // make direction 2d (x,z) and normalize
-                // then multiply by movement speed
-                var b = 1/(abs(direction.x)+abs(direction.z));
-                var directionX = movementSpeed*b*direction.x;
-                var directionZ = movementSpeed*b*direction.z;
-                if (forward) {
-                    nextPos.z = player1.scene.position.z + directionZ;
-                    nextPos.x = player1.scene.position.x + directionX;
-                    rotation = Math.atan2(directionX, directionZ)
-                }
-                if (backward) {
-                    nextPos.z = player1.scene.position.z - directionZ;
-                    nextPos.x = player1.scene.position.x - directionX;
-                    rotation = Math.atan2(directionX, directionZ) + Math.PI
-                }
-                if (left) {
-                    nextPos.z = player1.scene.position.z - directionX;
-                    nextPos.x = player1.scene.position.x + directionZ;
-                    rotation = Math.atan2(directionX, directionZ) + Math.PI/2
-                }
-                if (right) {
-                    nextPos.z = player1.scene.position.z + directionX;
-                    nextPos.x = player1.scene.position.x - directionZ;
-                    rotation = Math.atan2(directionX, directionZ) - Math.PI/2
-                }
-                player1.velocity.x = (nextPos.x-player1.scene.position.x)/delta
-                player1.velocity.z = (nextPos.z-player1.scene.position.z)/delta
-                nextPos.y = player1.scene.position.y // this is going to need to change for running up/down hill
-                movePlayer1(nextPos, rotation)
-                if (player1.state!='running') {
-                    mixer.clipAction(player1.animations[2]).fadeOut(0.5);
-                    var action = mixer.clipAction(player1.animations[1]).reset();
-                    action.fadeIn(0.2).play();
-                    player1.state = 'running'
-                }
-            } else if (player1.state=='running') {
-                mixer.clipAction(player1.animations[1]).fadeOut(0.5);
-                var action = mixer.clipAction(player1.animations[2]).reset();
-                action.fadeIn(0.2).play();
-                player1.state = 'standing'
-            }
-            if (space) {
-                nextPos = player1.scene.position.clone().add(player1.velocity.clone().multiplyScalar(delta))
-                movePlayer1(nextPos, rotation)
-            }
-        }
+        player1.animate(delta, input);
         // it might be inefficient to create this vector on every frame
         cameraTarget.copy(player1.scene.position.clone().add(new Vector3(0,1,0)))
         updateCamera(theta, phi)
@@ -100,78 +40,22 @@ function animate() {
     renderer.render( scene, camera );
 }
 
-function movePlayer1(nextPos, rotation) {
-    if(!collisionDetected(nextPos)){
-        movePlayer(player1, nextPos, rotation);
-        sendMessage(
-            {
-                player: playerUuid,
-                x: player1.scene.position.x,
-                y: player1.scene.position.y,
-                z: player1.scene.position.z,
-                rotation: rotation
-            }
-        )
-    } else {
-        player1.velocity.set(0,0,0)
-    }
-}
-
-var displayCollisionLines = true
-function collisionDetected(nextPos){
-    if (displayCollisionLines){
-        player1.scene.children.forEach((child) => {
-           if (child.name === "collision line") {
-               player1.scene.remove(child)
-           }
-        })
-    }
-    for(var a=-1; a<=1; a++){
-        for(var b=0; b<=1; b++){
-            for(var c=-1; c<=1; c++){
-                var vert = new Vector3(a, b, c);
-                vert = vert.clone().normalize()
-                var ray = new Raycaster(new Vector3(nextPos.x, 0, nextPos.z), vert);
-                if (displayCollisionLines){
-                    var geometry = new Geometry();
-                    geometry.vertices.push(
-                        vert,
-                        new Vector3()
-                    );
-                    var material = new LineBasicMaterial({
-                        color: 0xff0000
-                    });
-                    var line = new Line( geometry, material )
-                    line.name = "collision line"
-                    player1.scene.add(line);
-                }
-                // the true below denotes to recursivly check for collision with objects and all their children. Might not be efficient
-                var collisionResults = ray.intersectObjects(collidableEnvironment, true);
-                if ( collisionResults.length > 0 && collisionResults[0].distance <= new Line3(new Vector3(), vert).distance()) {
-                    return true
-                }
-            }
-        }
-    }
-    return false;
-}
-
 function toggleKey(event, toggle) {
     switch(event.key) {
         case 'w':
-            forward = toggle;
+            input.forward = toggle;
             break;
         case 'a':
-            left = toggle;
+            input.left = toggle;
             break;
         case 's':
-            backward = toggle;
+            input.backward = toggle;
             break;
         case 'd':
-            right = toggle;
+            input.right = toggle;
             break;
         case ' ':
-            space = toggle;
+            input.space = toggle;
             break;
     }
 }
@@ -196,8 +80,4 @@ function resize() {
     renderer.setSize(window.innerWidth, window.innerHeight);
     camera.aspect = window.innerWidth / window.innerHeight;
     camera.updateProjectionMatrix();
-}
-
-function abs(num) {
-    return Math.abs(num);
 }
--- client/src/player1.js
+++ client/src/player1.js
@@ -1,8 +1,11 @@
-import {Vector3, AnimationMixer, Raycaster, Line3} from 'three'
+import {Vector3, AnimationMixer, Raycaster, Line3, Geometry, LineBasicMaterial, Line} from 'three'
 
 import {loader} from './loader'
 import {uuid} from './utils'
 import {scene, collidableEnvironment} from './scene'
+import {camera} from './camera'
+import {movePlayer} from './players'
+import {sendMessage} from './websocket'
 
 import Adam from '../models/benji.glb'
 
@@ -39,7 +42,129 @@ loader.load( Adam, ( gltf ) => {
         return ret;
     }
 
-    var action = mixer.clipAction(player1.getAnimation("Idle")).reset();
+    var displayCollisionLines = true
+    player1.collisionDetected = function(nextPos){
+        if (displayCollisionLines){
+            player1.scene.children.forEach((child) => {
+               if (child.name === "collision line") {
+                   player1.scene.remove(child)
+               }
+            })
+        }
+        for(var a=-1; a<=1; a++){
+            for(var b=0; b<=1; b++){
+                for(var c=-1; c<=1; c++){
+                    var vert = new Vector3(a, b, c);
+                    vert = vert.clone().normalize()
+                    var ray = new Raycaster(new Vector3(nextPos.x, 0, nextPos.z), vert);
+                    if (displayCollisionLines){
+                        var geometry = new Geometry();
+                        geometry.vertices.push(
+                            vert,
+                            new Vector3()
+                        );
+                        var material = new LineBasicMaterial({
+                            color: 0xff0000
+                        });
+                        var line = new Line( geometry, material )
+                        line.name = "collision line"
+                        player1.scene.add(line);
+                    }
+                    // the true below denotes to recursivly check for collision with objects and all their children. Might not be efficient
+                    var collisionResults = ray.intersectObjects(collidableEnvironment, true);
+                    if ( collisionResults.length > 0 && collisionResults[0].distance <= new Line3(new Vector3(), vert).distance()) {
+                        return true
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
+    player1.move = function(nextPos, rotation){
+        if(!player1.collisionDetected(nextPos)){
+            movePlayer(player1, nextPos, rotation);
+            sendMessage(
+                {
+                    player: playerUuid,
+                    x: player1.scene.position.x,
+                    y: player1.scene.position.y,
+                    z: player1.scene.position.z,
+                    rotation: rotation
+                }
+            )
+        } else {
+            player1.velocity.set(0,0,0)
+        }
+    }
+
+    player1.animate = function(delta, input){
+        var nextPos = new Vector3();
+        var rotation;
+        if (player1.falling()) {
+            mixer.clipAction( player1.animations[1] ).fadeOut(0.2);
+            player1.state = 'falling'
+            player1.velocity.y -= delta*10
+            nextPos = player1.scene.position.clone().add(player1.velocity.clone().multiplyScalar(delta))
+            player1.move(nextPos, rotation)
+        } else {
+            player1.velocity.set(0,0,0)
+            if (input.space) {
+                player1.velocity.y = 5
+            }
+            if (input.forward || input.backward || input.left || input.right) {
+                var movementSpeed = 0.12;
+                var direction = new Vector3();
+                camera.getWorldDirection(direction)
+                // make direction 2d (x,z) and normalize
+                // then multiply by movement speed
+                var b = 1/(Math.abs(direction.x)+Math.abs(direction.z));
+                var directionX = movementSpeed*b*direction.x;
+                var directionZ = movementSpeed*b*direction.z;
+                if (input.forward) {
+                    nextPos.z = player1.scene.position.z + directionZ;
+                    nextPos.x = player1.scene.position.x + directionX;
+                    rotation = Math.atan2(directionX, directionZ)
+                }
+                if (input.backward) {
+                    nextPos.z = player1.scene.position.z - directionZ;
+                    nextPos.x = player1.scene.position.x - directionX;
+                    rotation = Math.atan2(directionX, directionZ) + Math.PI
+                }
+                if (input.left) {
+                    nextPos.z = player1.scene.position.z - directionX;
+                    nextPos.x = player1.scene.position.x + directionZ;
+                    rotation = Math.atan2(directionX, directionZ) + Math.PI/2
+                }
+                if (input.right) {
+                    nextPos.z = player1.scene.position.z + directionX;
+                    nextPos.x = player1.scene.position.x - directionZ;
+                    rotation = Math.atan2(directionX, directionZ) - Math.PI/2
+                }
+                player1.velocity.x = (nextPos.x-player1.scene.position.x)/delta
+                player1.velocity.z = (nextPos.z-player1.scene.position.z)/delta
+                nextPos.y = player1.scene.position.y // this is going to need to change for running up/down hill
+                player1.move(nextPos, rotation)
+                if (player1.state!='running') {
+                    mixer.clipAction(player1.animations[2]).fadeOut(0.5);
+                    var action = mixer.clipAction(player1.animations[1]).reset();
+                    action.fadeIn(0.2).play();
+                    player1.state = 'running'
+                }
+            } else if (player1.state=='running') {
+                mixer.clipAction(player1.animations[1]).fadeOut(0.5);
+                var action = mixer.clipAction(player1.animations[2]).reset();
+                action.fadeIn(0.2).play();
+                player1.state = 'standing'
+            }
+            if (input.space) {
+                nextPos = player1.scene.position.clone().add(player1.velocity.clone().multiplyScalar(delta))
+                player1.move(nextPos, rotation)
+            }
+        }
+    }
+
+    var action = mixer.clipAction(player1.animations[2]).reset();
     action.fadeIn(0.2).play();
     player1.state = 'standing'
 });
