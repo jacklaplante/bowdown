6d79d1758dab20496c7c5688141c231a9b7009cb
unknown <laplante.jack@gmail.com>
2019-08-06 21:15:32 -0400

    it got so much brighter in here

--- client/src/renderer.js
+++ client/src/renderer.js
@@ -4,5 +4,7 @@ var renderer = new WebGLRenderer();
 renderer.setClearColor("#e5e5e5");
 renderer.setPixelRatio( window.devicePixelRatio );
 renderer.setSize( window.innerWidth, window.innerHeight );
+renderer.gammaOutput = true;
+renderer.gammaFactor = 2.2;
 
 export  {renderer }
