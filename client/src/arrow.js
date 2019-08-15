b2e0f7c48fbb3ad2fbbf3026b1b44b703b10f6dd
Jack LaPlante <laplante.jack@gmail.com>
2019-08-15 19:48:55 -0400

    working on arrows

--- client/src/arrow.js
+++ client/src/arrow.js
@@ -1,7 +1,8 @@
-import {BoxGeometry, MeshBasicMaterial, Mesh, Vector3} from 'three'
+import {BoxGeometry, MeshBasicMaterial, Mesh, Vector3, Raycaster} from 'three'
 
-import {scene} from './scene'
+import {scene, collidableEnvironment} from './scene'
 import {player1} from './player1'
+import {camera} from './camera'
 
 var arrows = []
 
@@ -9,11 +10,25 @@ function createArrow(){
     var geometry = new BoxGeometry(0.02, 0.02, 0.75);
     var material = new MeshBasicMaterial( {color: 0x00ff00} );
     var arrow = new Mesh( geometry, material );
+
+    // change this
     arrow.position.copy(player1.scene.position).add(new Vector3(0,1,0))
-    arrow.rotation.copy(player1.scene.rotation)
-    var direction = new Vector3()
-    player1.scene.getWorldDirection(direction)
-    arrow.velocity = direction.normalize().multiplyScalar(10)
+    arrow.rotation.copy(camera.rotation)
+
+    // if the reticle (center of screen) is pointed at something, aim arrows there! otherwise estimate where the player is aiming 
+    var raycaster = new Raycaster()
+    raycaster.setFromCamera({x: 0, y: 0}, camera)
+    var collisions = raycaster.intersectObjects(collidableEnvironment, true)
+    var origin = player1.scene.position.clone() // you will want to change this to be more accurate to the bow
+    var direction;
+    if (collisions.length > 0) {
+        direction = collisions[0].point.sub(player1.scene.position)
+    } else {
+        direction = new Vector3()
+        camera.getWorldDirection(direction)
+    }
+
+    arrow.velocity = direction.normalize().multiplyScalar(30)
     scene.add( arrow );
     arrows.push(arrow)
 }
@@ -24,6 +39,7 @@ function shootArrow(){
 
 function animateArrows(delta) {
     arrows.forEach((arrow) => {
+        arrow.velocity.y -= delta*9
         arrow.position.add(arrow.velocity.clone().multiplyScalar(delta))
     })
 }
