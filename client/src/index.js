8368c456b14f60f90b885ce352f2417185d2b936
Jack LaPlante <laplantej@google.com>
2019-08-26 15:09:55 -0400

    added lazy loading to start screen

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,13 @@
-import {start} from './game'
-
 const body = document.getElementsByTagName("BODY")[0];
 
 var startButton = document.createElement("div");
 startButton.setAttribute("style", "height: 100px; width: 100px; position: fixed; color: aquamarine; font-size: 30pt;")
-startButton.innerText = "START"
-startButton.onclick = function() {
-    start(body)
-    body.removeChild(startButton)
-}
+startButton.innerText = "LOADING"
 body.appendChild(startButton)
+import(/* webpackChunkName: "game" */ './game').then(function(module) {
+    startButton.innerText = "START"
+    startButton.onclick = function() {
+        body.removeChild(startButton)
+        module.start(body);
+    }
+})
