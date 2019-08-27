3db1a39a5a3855e027f9b18ac1c400ca1258596d
Jack LaPlante <laplantej@google.com>
2019-08-27 11:14:42 -0400

    production changes

--- client/webpack.common.js
+++ client/webpack.common.js
@@ -1,12 +1,7 @@
 const path = require('path');
 
 module.exports = {
-    mode: 'development',
     entry: './src/index.js',
-    devtool: 'inline-source-map',
-    devServer: {
-        contentBase: './dist'
-    },
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist')
