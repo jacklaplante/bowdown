eaf0dfda0b51fd19d490b6508521cc757725c45e
unknown <laplante.jack@gmail.com>
2019-09-01 10:55:56 -0400

    changed menu colors

--- client/src/index.js
+++ client/src/index.js
@@ -21,7 +21,7 @@ var startButton = document.getElementById("start");
 startButton.innerText = "LOADING"
 import(/* webpackChunkName: "game" */ './game').then(function(module) {
     startButton.innerText = "START"
-    startButton.setAttribute("style", "color: #611313; background: aliceblue; width: 35vw")
+    startButton.setAttribute("style", "color: #134461; background: #ffdba5; width: 35vw")
     startButton.onclick = function() {
         document.body.removeChild(document.getElementById("menu"))
         module.start();
