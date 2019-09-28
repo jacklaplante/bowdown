2f262037c81e0ea200fde9114c1c5d8f9054c7ba
unknown <laplante.jack@gmail.com>
2019-09-28 19:26:03 -0400

    changed skybox

--- client/src/scene.js
+++ client/src/scene.js
@@ -2,12 +2,12 @@ import { Scene, HemisphereLight, DirectionalLight, TextureLoader, MeshBasicMater
 
 import { loader } from './loader'
 import env from '../models/battle_planet.glb'
-import heather_ft from '../skybox/heather_ft.jpg'
-import heather_bk from '../skybox/heather_bk.jpg'
-import heather_up from '../skybox/heather_up.jpg'
-import heather_dn from '../skybox/heather_dn.jpg'
-import heather_rt from '../skybox/heather_rt.jpg'
-import heather_lf from '../skybox/heather_lf.jpg'
+import yellowcloud_ft from '../skybox/yellowcloud_ft.jpg'
+import yellowcloud_bk from '../skybox/yellowcloud_bk.jpg'
+import yellowcloud_up from '../skybox/yellowcloud_up.jpg'
+import yellowcloud_dn from '../skybox/yellowcloud_dn.jpg'
+import yellowcloud_rt from '../skybox/yellowcloud_rt.jpg'
+import yellowcloud_lf from '../skybox/yellowcloud_lf.jpg'
 
 var scene = new Scene();
 var collidableEnvironment = []
@@ -20,12 +20,12 @@ loader.load(env, function (gltf) {
 });
 
 let materialArray = [];  
-materialArray.push(new MeshBasicMaterial( { map: new TextureLoader().load(heather_ft) }));
-materialArray.push(new MeshBasicMaterial( { map: new TextureLoader().load(heather_bk) }));
-materialArray.push(new MeshBasicMaterial( { map: new TextureLoader().load(heather_up) }));
-materialArray.push(new MeshBasicMaterial( { map: new TextureLoader().load(heather_dn) }));
-materialArray.push(new MeshBasicMaterial( { map: new TextureLoader().load(heather_rt) }));
-materialArray.push(new MeshBasicMaterial( { map: new TextureLoader().load(heather_lf) }));
+materialArray.push(new MeshBasicMaterial( { map: new TextureLoader().load(yellowcloud_ft) }));
+materialArray.push(new MeshBasicMaterial( { map: new TextureLoader().load(yellowcloud_bk) }));
+materialArray.push(new MeshBasicMaterial( { map: new TextureLoader().load(yellowcloud_up) }));
+materialArray.push(new MeshBasicMaterial( { map: new TextureLoader().load(yellowcloud_dn) }));
+materialArray.push(new MeshBasicMaterial( { map: new TextureLoader().load(yellowcloud_rt) }));
+materialArray.push(new MeshBasicMaterial( { map: new TextureLoader().load(yellowcloud_lf) }));
 for (let i = 0; i < 6; i++)
   materialArray[i].side = BackSide;
 let skyboxGeo = new BoxGeometry(1000, 1000, 1000);
