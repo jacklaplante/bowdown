f456742b5053537a4fc6352a07b3a0afe323232c
unknown <laplante.jack@gmail.com>
2019-10-18 12:41:24 -0400

    king of crown cone

--- client/src/kingOfCrown.js
+++ client/src/kingOfCrown.js
@@ -1,19 +1,29 @@
-import {Geometry, LineBasicMaterial, Line} from 'three'
+import {Geometry, LineBasicMaterial, Line, ConeGeometry, MeshBasicMaterial, Mesh} from 'three'
 
 import {scene} from './scene'
 
 function updateCrown(player) {
     if (player.kingOfCrown) {
-        if (player.kingOfCrownPointer) {
-            scene.remove(player.kingOfCrownPointer)
+        if (player.kingOfCrownLine) {
+            scene.remove(player.kingOfCrownLine)
         }
-        var geometry = new Geometry();
-        var material = new LineBasicMaterial({color: 0xfffae8, linewidth: 20});
+        if (!player.kingOfCrownCone) {
+            var coneGeometry = new ConeGeometry(0.5, 1, 9)
+            var coneMaterial = new MeshBasicMaterial({color: 0xff5900})
+            var cone = new Mesh(coneGeometry, coneMaterial)
+            player.kingOfCrownCone = cone
+            scene.add(cone)
+        }
         var pos = player.gltf.scene.position.clone()
-        geometry.vertices.push(pos, pos.clone().multiplyScalar(500));
-        var line = new Line(geometry, material)
+        var lineGeometry = new Geometry();
+        var lineMaterial = new LineBasicMaterial({color: 0xff5900, linewidth: 20});
+        lineGeometry.vertices.push(pos, pos.clone().multiplyScalar(500));
+        var line = new Line(lineGeometry, lineMaterial)
         scene.add(line)
-        player.kingOfCrownPointer = line
+        player.kingOfCrownLine = line
+        
+        player.kingOfCrownCone.position.copy(pos)
+        player.kingOfCrownCone.rotation.copy(player.gltf.scene.rotation)
     }
 }
 
