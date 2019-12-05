625e573370ff26d9b2783d056b8d3232cf2c0ca3
Jack LaPlante <laplante.jack@gmail.com>
2019-12-04 22:46:03 -0500

    orc is an object3d on the server now but I cant figure out how to get him to face me

--- server/entities.js
+++ server/entities.js
@@ -8,7 +8,7 @@ const content = fs.readFileSync('./lowildNavMesh.glb');
 global.THREE = THREE
 
 const entities = {};
-const orc = {}
+const orc = new THREE.Object3D();
 let games, payloads
 
 const loader = new THREE.GLTFLoader();
@@ -27,9 +27,11 @@ entities.init = function(g, p, gN) {
             if (node.isMesh) navmesh = node;
         });
         pathfinding.setZoneData(ZONE, Pathfinding.createZone(navmesh.geometry));
-        orc.position = pathfinding.zones[ZONE].vertices[Math.round(pathfinding.zones[ZONE].vertices.length/2)] // random vector on nav mesh
+        orc.position.copy(pathfinding.zones[ZONE].vertices[Math.round(pathfinding.zones[ZONE].vertices.length/2)]) // random vector on nav mesh
+        let quat = new THREE.Quaternion().setFromUnitVectors(new THREE.Vector3(0,1,0), orc.position.clone().normalize())
+        orc.applyQuaternion(quat)
+        orc.velocity = new THREE.Vector3(1,0,0).applyQuaternion(quat) // this is important for the initial rotation
         orc.target = orc.position.clone()
-
         entities.go()
     }, ( e ) => {
         console.error( e );
@@ -49,12 +51,12 @@ entities.go = function() {
         if (orc.path && orc.path.length > 0) {
             let direction = orc.path[0].clone().sub(orc.position);
             if (direction.lengthSq() > 0.05 * 0.05) {
-                var nextPos = orc.position.clone().add(direction.clone().normalize().multiplyScalar(Math.min(getMovementSpeed(), direction.length()))); // either go at the speed to the next point in path or to the next point if the speed will make the orc go past the point
-                orc.velocity = nextPos.clone().sub(orc.position).multiplyScalar(1/updateSpeed)
-                orc.position = nextPos
-                orc.rotation = new THREE.Euler().setFromQuaternion(
-                    new THREE.Quaternion().setFromUnitVectors(new THREE.Vector3(0,1,0), orc.position.clone().normalize())
-                    )
+                let nextPos = orc.position.clone().add(direction.clone().normalize().multiplyScalar(Math.min(getMovementSpeed(), direction.length()))); // either go at the speed to the next point in path or to the next point if the speed will make the orc go past the point
+                // orc.applyQuaternion(new THREE.Quaternion().setFromUnitVectors(orc.velocity.clone().normalize(), direction.clone().normalize()))
+                // orc.rotateOnAxis(new THREE.Vector3(0,1,0), orc.velocity.clone().normalize().angleTo(direction.clone().normalize()))
+                orc.velocity.copy(nextPos.clone().sub(orc.position).multiplyScalar(1/updateSpeed))
+                orc.applyQuaternion(new THREE.Quaternion().setFromUnitVectors(orc.position.clone().normalize(), nextPos.clone().normalize()))
+                orc.position.copy(nextPos)
             } else {
                 orc.path.shift();
             }
