a5d780a5ae44cc4105dbf2b978892008fd71598d
unknown <laplante.jack@gmail.com>
2019-12-05 21:31:24 -0500

    orc pit

--- server/entities.js
+++ server/entities.js
@@ -3,7 +3,7 @@ const THREE = global.THREE = require('three');
 global.TextDecoder = require('util').TextDecoder;
 require('three/examples/js/loaders/GLTFLoader.js');
 const { Pathfinding } = require('three-pathfinding');
-const content = fs.readFileSync('./lowildNavMesh.glb');
+const content = fs.readFileSync('./orc_pit_NavMesh.glb');
 
 global.THREE = THREE
 
@@ -31,7 +31,6 @@ entities.init = function(g, p, gN) {
         let quat = new THREE.Quaternion().setFromUnitVectors(new THREE.Vector3(0,1,0), orc.position.clone().normalize())
         orc.applyQuaternion(quat)
         orc.velocity = new THREE.Vector3(0,0,1).applyQuaternion(quat) // this is important for the initial rotation
-        orc.target = orc.position.clone()
         entities.go()
     }, ( e ) => {
         console.error( e );
@@ -44,22 +43,26 @@ entities.go = function() {
     setInterval( () => {
         if (Object.entries(games[gameName].players).length == 0) return
         let pos = toVector(games[gameName].players[Object.keys(games[gameName].players)[0]].position)
-        if (!orc.target.equals(pos)) {
-            orc.target = pos
-            orc.path = pathfinding.findPath(orc.position, pos, ZONE, pathfinding.getGroup(ZONE, orc.position));
-        }
-        if (orc.path && orc.path.length > 0) {
-            let direction = orc.path[0].clone().sub(orc.position);
-            if (direction.lengthSq() > 0.05 * 0.05) {
-                let nextPos = orc.position.clone().add(direction.clone().normalize().multiplyScalar(Math.min(getMovementSpeed(), direction.length()))); // either go at the speed to the next point in path or to the next point if the speed will make the orc go past the point
-                orc.applyQuaternion(new THREE.Quaternion().setFromUnitVectors(orc.velocity.clone().projectOnPlane(nextPos.clone().normalize()).normalize(), direction.clone().projectOnPlane(nextPos.clone().normalize()).normalize()))
-                orc.velocity.copy(nextPos.clone().sub(orc.position).multiplyScalar(1/updateSpeed))
-                orc.applyQuaternion(new THREE.Quaternion().setFromUnitVectors(orc.position.clone().normalize(), nextPos.clone().normalize()))
-                orc.position.copy(nextPos)
-            } else {
-                orc.path.shift();
+        if (pathfinding.getClosestNode(pos, ZONE, getGroupId())) { // not sure if this works
+            if (orc.target == null || !orc.target.equals(pos)) {
+                orc.target = pos
+                orc.path = pathfinding.findPath(orc.position, pos, ZONE, getGroupId());
             }
-            entities.updateState()
+            if (orc.path && orc.path.length > 0) {
+                let direction = orc.path[0].clone().sub(orc.position);
+                if (direction.lengthSq() > 0.05 * 0.05) {
+                    let nextPos = orc.position.clone().add(direction.clone().normalize().multiplyScalar(Math.min(getMovementSpeed(), direction.length()))); // either go at the speed to the next point in path or to the next point if the speed will make the orc go past the point
+                    orc.applyQuaternion(new THREE.Quaternion().setFromUnitVectors(orc.velocity.clone().projectOnPlane(nextPos.clone().normalize()).normalize(), direction.clone().projectOnPlane(nextPos.clone().normalize()).normalize()))
+                    orc.velocity.copy(nextPos.clone().sub(orc.position).multiplyScalar(1/updateSpeed))
+                    orc.applyQuaternion(new THREE.Quaternion().setFromUnitVectors(orc.position.clone().normalize(), nextPos.clone().normalize()))
+                    orc.position.copy(nextPos)
+                } else {
+                    orc.path.shift();
+                }
+                entities.updateState()
+            }
+        } else {
+            orc.target = null
         }
     }, updateSpeed);
 }
@@ -76,6 +79,10 @@ entities.updateState = function() {
         velocity: orc.velocity,
         rotation: orc.rotation
     }
+}
+
+function getGroupId() {
+    return pathfinding.getGroup(ZONE, orc.position)
 }
 
 function getMovementSpeed() {
