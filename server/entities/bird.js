0caf8bf2da4facb15e4dde8e19f220dccf206c6c
Jack LaPlante <laplante.jack@gmail.com>
2019-12-09 16:09:22 -0500

    bird rotation is working

--- server/entities/bird.js
+++ server/entities/bird.js
@@ -3,17 +3,20 @@ const THREE = global.THREE = require('three');
 const bird = new THREE.Object3D();
 
 bird.init = function() {
-  bird.position.set(-90,-90,-90)
-  bird.velocity = new THREE.Vector3();
-  let quat = new THREE.Quaternion().setFromUnitVectors(new THREE.Vector3(0,1,0), bird.position.clone().normalize())
-  bird.applyQuaternion(quat)
+  this.position.set(-90,-90,-90)
+  this.velocity = new THREE.Vector3();
+  let quat = new THREE.Quaternion().setFromUnitVectors(new THREE.Vector3(0,1,0), this.position.clone().normalize())
+  this.applyQuaternion(quat)
+  this.velocity = new THREE.Vector3(0,0,1).applyQuaternion(quat) // this is important for the initial rotation
 }
 
 bird.update = function() {
-  let nextPos = bird.position.clone().applyAxisAngle(new THREE.Vector3(1,0,0), 0.01)
-  bird.velocity.copy(nextPos.clone().sub(this.position))
-  bird.applyQuaternion(new THREE.Quaternion().setFromUnitVectors(bird.position.clone().normalize(), nextPos.clone().normalize()))
-  bird.position.copy(nextPos)
+  let nextPos = this.position.clone().applyAxisAngle(new THREE.Vector3(1,0,0), 0.01)
+  let nextVel = nextPos.clone().sub(this.position)
+  this.applyQuaternion(new THREE.Quaternion().setFromUnitVectors(this.velocity.clone(), nextVel.clone()))
+  this.velocity.copy(nextVel)
+  this.applyQuaternion(new THREE.Quaternion().setFromUnitVectors(bird.position.clone().normalize(), nextPos.clone().normalize()))
+  this.position.copy(nextPos)
 }
 
 bird.getState = function() {
