198223ea26247a9bc69ef3e06143cf1e3b25b223
Jack LaPlante <laplante.jack@gmail.com>
2020-01-06 09:04:35 -0500

    made constants module for controls text

---
+++ client/src/constants.js
@@ -0,0 +1,31 @@
+const touchControlsText = [
+  "mobile controls",
+  "Move",
+  "  Touch movement on left side of screen",
+  "Move Camera",
+  "  Touch movement on right side of screen",
+  "shoot Arrow",
+  "  Target button (hold -> release)",
+  "shoot Grapple",
+  "  Grapple button (hold -> release)",
+  "Jump",
+  "  Green bar on bottom-right of screen"
+]
+
+const mouseKeyboardControlsText = [
+  "mouse + keyboard controls",
+  "move camera",
+  "  mouse",
+  "shoot arrow",
+  "  Left click (hold -> release)",
+  "shoot grapple",
+  "  Right click (hold -> release)",
+  "move",
+  "  WASD",
+  "jump",
+  "  space",
+  "sprint",
+  "  Shift"
+]
+
+export {touchControlsText, mouseKeyboardControlsText}
--- client/src/scene/lobby.js
+++ client/src/scene/lobby.js
@@ -1,6 +1,7 @@
 import {BoxGeometry, Mesh, MeshStandardMaterial, DoubleSide, Group, PlaneBufferGeometry} from 'three'
 
 import {createTextMesh} from '../utils'
+import {touchControlsText, mouseKeyboardControlsText} from '../constants'
 
 const lobby = new Group()
 
@@ -24,35 +25,9 @@ var controlsMesh
 lobby.showControls = function() {
   let text
   if (window.usingTouchControls) {
-    text = [
-      "mobile controls",
-      "Move",
-      "  Touch movement on left side of screen",
-      "Move Camera",
-      "  Touch movement on right side of screen",
-      "shoot Arrow",
-      "  Target button (hold -> release)",
-      "shoot Grapple",
-      "  Grapple button (hold -> release)",
-      "Jump",
-      "  Green bar on bottom-right of screen"
-    ]
+    text = touchControlsText
   } else {
-    text = [
-      "mouse + keyboard controls",
-      "move camera",
-      "  mouse",
-      "shoot arrow",
-      "  Left click (hold -> release)",
-      "shoot grapple",
-      "  Right click (hold -> release)",
-      "move",
-      "  WASD",
-      "jump",
-      "  space",
-      "sprint",
-      "  Shift"
-    ]
+    text = mouseKeyboardControlsText
   }
   menu.remove(controlsMesh)
   controlsMesh = createTextMesh(text, 0x0974A5)
